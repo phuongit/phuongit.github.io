53afd58c02f2266f1d0f6fac478817b83e6ac15a
phuongit <phuongk3a@gmail.com>
2019-09-24 16:29:14 +0700

    Add files via upload

--- movie.service.ts
+++ movie.service.ts
@@ -3,6 +3,8 @@ Mr Nguyen Duc Hoang
 https://www.youtube.com/c/nguyenduchoang
 Email: sunlight4d@gmail.com
 Services could get data from server, create fake data, get data from local storage
+
+
 */
 import { Injectable } from '@angular/core';
 // import { fakeMovies } from './fake-movies';
